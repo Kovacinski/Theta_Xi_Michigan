7ae7c85dd4f63e378470b63fdabf46bb0543058b
Kovacinski <kova@umich.edu>
2015-08-20 20:35:28 +0000

    public files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('ThetaXiApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+  $routeProvider
+  	.when('/', {
+    templateUrl: 'views/Home.html'
+  })
+  .when('/Home',{
+    templateUrl: 'views/Home.html'
+  })
+  .when('/Recruitment',{
+    	templateUrl: 'views/Recruitment.html'
+  })
+    .when('/Brotherhood',{
+    templateUrl: 'views/Brotherhood.html'
+  })
+  .when('/Alumni',{
+    templateUrl:'views/Alumni.html'
+  })
+    .otherwise({
+    redirectTo: '/'
+  });
+});
